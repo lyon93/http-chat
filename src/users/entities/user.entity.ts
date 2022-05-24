4fb9d39681bb8fb717a91cf33d19f4bbff793647
lyon93 <lyonleon93@gmail.com>
2022-05-25 00:11:37 +0200

    add user fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { MinLength } from 'class-validator';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column()
+  @MinLength(8)
+  password: string;
+}
