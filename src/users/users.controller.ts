cd28c62458557f70eb69f046a1333b24242cf3fc
lyon93 <lyonleon93@gmail.com>
2022-05-25 00:36:13 +0200

    feature: avoid duplicated emails

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user: User = await this.usersService.findByEmail(createUserDto.email);
+    if (user) {
+      throw new BadRequestException('This email already taken');
+    }
     return this.usersService.create(createUserDto);
   }
 
