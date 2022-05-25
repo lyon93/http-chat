288e16d4576385787d354bd6b01ce9a985fb28a8
lyon93 <lyonleon93@gmail.com>
2022-05-25 21:25:31 +0200

    feature: add login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { User } from './../users/entities/user.entity';
 import { UsersService } from './../users/users.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user: User = await this.usersService.findByEmail(email);
@@ -13,5 +17,11 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
