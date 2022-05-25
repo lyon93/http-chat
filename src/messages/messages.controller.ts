e928ad3405fd3cf67192f1e34c2bdacd4dd6ccdf
lyon93 <lyonleon93@gmail.com>
2022-05-26 00:15:46 +0200

    fix: remove unused imports

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
