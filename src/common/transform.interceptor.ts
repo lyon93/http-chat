8d404339666dba3782d46f8d448e9eb3e40ad782
lyon93 <lyonleon93@gmail.com>
2022-05-25 19:14:28 +0200

    feature: add interceptor

---
+++ src/common/transform.interceptor.ts
@@ -0,0 +1,15 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { instanceToPlain } from 'class-transformer';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  }
+}
