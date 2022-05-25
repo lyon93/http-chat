78d20c18fa292d2582a7af98af9bac3cde92d529
lyon93 <lyonleon93@gmail.com>
2022-05-25 19:43:46 +0200

    fix format

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,7 +10,7 @@ export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
-  ) { }
+  ) {}
 
   create(createUserDto: CreateUserDto): Promise<User> {
     const user = Object.assign({}, createUserDto);
