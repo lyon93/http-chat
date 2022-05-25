ec7149d3cdc226e14d08a45957d7650ecc1efd02
lyon93 <lyonleon93@gmail.com>
2022-05-26 00:31:45 +0200

    feature: add user - message relation

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,1 +1,14 @@
-export class Message {}
+import { User } from './../../users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  body: string;
+
+  @ManyToOne(() => User, (user) => user.messages)
+  user: User;
+}
