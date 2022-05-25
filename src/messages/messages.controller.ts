50f6c624498cc4bc490c0af32e8be36e66ea9622
lyon93 <lyonleon93@gmail.com>
2022-05-26 00:16:03 +0200

    fix: imports

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
 
 @Controller('messages')
 export class MessagesController {
