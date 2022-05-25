decef8c30519b8e5fb5817900db948bc0500607b
lyon93 <lyonleon93@gmail.com>
2022-05-25 20:15:07 +0200

    feature: import user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
+import { UsersModule } from './../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
