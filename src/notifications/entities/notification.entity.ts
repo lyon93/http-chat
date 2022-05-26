9d0ea58c982e9482794136f0a0ce382e02004bb3
lyon93 <lyonleon93@gmail.com>
2022-05-26 19:58:17 +0200

    feature: add notification resource

---
+++ src/notifications/entities/notification.entity.ts
@@ -0,0 +1,25 @@
+import { Exclude } from 'class-transformer';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Notification {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  senderEmail: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @ManyToOne(() => User, (user) => user.notifications)
+  @Exclude()
+  user: User;
+}
